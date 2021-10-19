35c332f4004c659afdf8d9d411ace18431869970
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-10-19 11:56:59 +0900

    Initial commit

---
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,9 @@
+const hello = function(name, age) {
+  // let name = "Code Mafia";
+  console.log("Hello" + name + age);
+  return name + age
+}
+
+hello('Code Mafia', 20);
+const returnVal = hello('Code Mafia', 10);
+console.log(returnVal)
---
+++ 04_JavaScriptの基礎を固める/040_配列の使い方/start/main.js
@@ -0,0 +1,6 @@
+const arry = [1,2,3,4,5,6, 'moji', false];
+arry.unshift('new item');
+const val = arry.shift();
+
+console.log(val)
+console.log(arry)
